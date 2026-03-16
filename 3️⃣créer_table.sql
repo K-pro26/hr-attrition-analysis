79a936bfd7f3d22573a2c28fb033b85027653c7f
K-pro26 <m0506272775@proton.me>
2026-03-16 21:33:20 +0000

    Create 3️⃣créer_table.sql

---
+++ 3️⃣créer_table.sql
@@ -0,0 +1,16 @@
+CREATE TABLE employees (
+employee_id INT,
+age INT,
+gender VARCHAR(10),
+department VARCHAR(50),
+job_role VARCHAR(50),
+salary INT,
+years_at_company INT,
+job_satisfaction INT,
+work_life_balance INT,
+performance_rating INT,
+overtime VARCHAR(10),
+education_level VARCHAR(20),
+marital_status VARCHAR(20),
+attrition VARCHAR(10)
+);
